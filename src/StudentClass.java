d27808a67e8f95ceb41698c6eda1e303833ce34f
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-02-06 10:02:09 +0530

    updated

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -16,7 +16,8 @@ class Student {
 
     void getstudent()
     {
-        System.out.println("Name: "+name+" Roll :"+roll_no);
+        System.out.print("Name: "+name+" Roll :"+roll_no);
+        System.out.println("");
     }
 }
 
